2f613bfb66a5144679820fe9af29c750032c9873
nikit <nikit@DESKTOP-5363RTP>
2023-05-02 17:04:07 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car {
+private:
+    int x;
+    int y;
+public:
+    Car(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    void move() {
+        x=x+3;
+        y=y+1;
+    }
+    int getX() {
+        return x;
+    }
+    int getY() {
+        return y;
+    }
+
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    Car car(0, 0);
+    for (int i = 0; i < 7; i++) {
+        car.move();
+        cout << "X: " << car.getX() << "\t Y: " << car.getY() << endl;
+    }
+    return 0;
+}
